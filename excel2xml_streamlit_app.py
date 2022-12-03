f3e11dcd254252c0ac5cb1c8303bf8e5a7e3e94e
zK <kmerrill2790@gmail.com>
2022-12-02 22:52:01 -0800

    Update excel2xml_streamlit_app.py

--- excel2xml_streamlit_app.py
+++ excel2xml_streamlit_app.py
@@ -21,76 +21,79 @@ with col2:
     )
 
 uploaded_file = st.file_uploader("Create XML")
-if uploaded_file is not None:
-    dataframe = pd.read_excel(uploaded_file)
-    tree = et.parse(f'TEMPLATES/iTunes_TV_EPISODE_TEMPLATE_v5-3_{option}.xml')
-    template_root = tree.getroot()
-    package_folder = "iTunes Package with XML"
-    os.makedirs(package_folder)
-    xml_folder = "XML"
-    os.makedirs(xml_folder)
+try:
+    if uploaded_file is not None:
+        dataframe = pd.read_excel(uploaded_file)
+        tree = et.parse(f'TEMPLATES/iTunes_TV_EPISODE_TEMPLATE_v5-3_{option}.xml')
+        template_root = tree.getroot()
+        package_folder = "iTunes Package with XML"
+        os.makedirs(package_folder)
+        xml_folder = "XML"
+        os.makedirs(xml_folder)
 
-    for index, row in dataframe.iterrows():
-        if index > 2:
-            package_name = str(row['Unnamed: 23'])
-            full_sale_date = str(row['Unnamed: 34'])
-            zip_name = str(row['ITUNES'])
-            
-            if "en" in option:
-                template_root[2][16][0][0][1].text = package_name+'.mov'#mov file name
-                template_root[2][16][0][1][1].text = package_name+'.scc'#scc file name
-                template_root[2][18][0][1].text = full_sale_date[0:10]
-            else:
-                template_root[2][15][0][0][1].text = package_name+".mov"#mov file name
-                template_root[2][17][0][1].text = full_sale_date[0:10]
-            rating = template_root[2][14][0].attrib
-            rating['code'] = str(row['Unnamed: 7'])
-            
-            for container_id in template_root[2].iter('{http://apple.com/itunes/importer}container_id'):
-                container_id.text = str(row['ITUNES'])
-                
-            for container_position in template_root[2].iter('{http://apple.com/itunes/importer}container_position'):
-                container_position.text = str(row['Unnamed: 24'])
-                
-            for vendor_id in template_root[2].iter('{http://apple.com/itunes/importer}vendor_id'):
-                vendor_id.text = str(row['Unnamed: 23'])
-            
-            for episode_production_number in template_root[2].iter('{http://apple.com/itunes/importer}episode_production_number'):
-                episode_production_number.text = str(row['TITLE'])
-            
-            for title in template_root[2].iter('{http://apple.com/itunes/importer}title'):
-                title.text = str(row['Unnamed: 3'])
-                
-            for studio_release_title in template_root[2].iter('{http://apple.com/itunes/importer}studio_release_title'):
-                studio_release_title.text = str(row['Unnamed: 4'])
-            
-            for description in template_root[2].iter('{http://apple.com/itunes/importer}description'):
-                description.text = str(row['Unnamed: 5'])
-            
-            for release_date in template_root[2].iter('{http://apple.com/itunes/importer}release_date'):
-                full_date = str(row['Unnamed: 14'])
-                release_date.text = full_date[0:10]
-                
-            for copyright in template_root[2].iter('{http://apple.com/itunes/importer}copyright_cline'):
-                copyright.text = str(row['Unnamed: 15'])
+        for index, row in dataframe.iterrows():
+            if index > 2:
+                package_name = str(row['Unnamed: 23'])
+                full_sale_date = str(row['Unnamed: 34'])
+                zip_name = str(row['ITUNES'])
 
-            package = f'{package_name}.itmsp'
-            xml = "metadata.xml"
-            os.makedirs(package)
-            package_path = os.path.abspath(package)    
-            tree.write(f'{package_name}.xml', encoding="utf-8", xml_declaration=True)
-            tree.write(xml, encoding="utf-8", xml_declaration=True)
-            xml_path = os.path.abspath(xml)
-            shutil.move(xml_path, package_path)
-            shutil.move(os.path.abspath(package_path), os.path.abspath(package_folder))
-            shutil.move(os.path.abspath(f'{package_name}.xml'), os.path.abspath(xml_folder))
+                if "en" in option:
+                    template_root[2][16][0][0][1].text = package_name+'.mov'#mov file name
+                    template_root[2][16][0][1][1].text = package_name+'.scc'#scc file name
+                    template_root[2][18][0][1].text = full_sale_date[0:10]
+                else:
+                    template_root[2][15][0][0][1].text = package_name+".mov"#mov file name
+                    template_root[2][17][0][1].text = full_sale_date[0:10]
+                rating = template_root[2][14][0].attrib
+                rating['code'] = str(row['Unnamed: 7'])
 
-zip_name = container_id.text
-os.mkdir(zip_name)
-shutil.move(os.path.abspath(package_folder), os.path.abspath(zip_name))
-shutil.move(os.path.abspath(xml_folder), os.path.abspath(zip_name))
-shutil.make_archive(zip_name, 'zip', os.path.abspath(zip_name))
-shutil.rmtree(zip_name)
+                for container_id in template_root[2].iter('{http://apple.com/itunes/importer}container_id'):
+                    container_id.text = str(row['ITUNES'])
 
-with open(zip_name+'.zip', 'rb') as f:
-    st.download_button('Download Zip', f, file_name=zip_name+'.zip')
+                for container_position in template_root[2].iter('{http://apple.com/itunes/importer}container_position'):
+                    container_position.text = str(row['Unnamed: 24'])
+
+                for vendor_id in template_root[2].iter('{http://apple.com/itunes/importer}vendor_id'):
+                    vendor_id.text = str(row['Unnamed: 23'])
+
+                for episode_production_number in template_root[2].iter('{http://apple.com/itunes/importer}episode_production_number'):
+                    episode_production_number.text = str(row['TITLE'])
+
+                for title in template_root[2].iter('{http://apple.com/itunes/importer}title'):
+                    title.text = str(row['Unnamed: 3'])
+
+                for studio_release_title in template_root[2].iter('{http://apple.com/itunes/importer}studio_release_title'):
+                    studio_release_title.text = str(row['Unnamed: 4'])
+
+                for description in template_root[2].iter('{http://apple.com/itunes/importer}description'):
+                    description.text = str(row['Unnamed: 5'])
+
+                for release_date in template_root[2].iter('{http://apple.com/itunes/importer}release_date'):
+                    full_date = str(row['Unnamed: 14'])
+                    release_date.text = full_date[0:10]
+
+                for copyright in template_root[2].iter('{http://apple.com/itunes/importer}copyright_cline'):
+                    copyright.text = str(row['Unnamed: 15'])
+
+                package = f'{package_name}.itmsp'
+                xml = "metadata.xml"
+                os.makedirs(package)
+                package_path = os.path.abspath(package)    
+                tree.write(f'{package_name}.xml', encoding="utf-8", xml_declaration=True)
+                tree.write(xml, encoding="utf-8", xml_declaration=True)
+                xml_path = os.path.abspath(xml)
+                shutil.move(xml_path, package_path)
+                shutil.move(os.path.abspath(package_path), os.path.abspath(package_folder))
+                shutil.move(os.path.abspath(f'{package_name}.xml'), os.path.abspath(xml_folder))
+
+    zip_name = container_id.text
+    os.mkdir(zip_name)
+    shutil.move(os.path.abspath(package_folder), os.path.abspath(zip_name))
+    shutil.move(os.path.abspath(xml_folder), os.path.abspath(zip_name))
+    shutil.make_archive(zip_name, 'zip', os.path.abspath(zip_name))
+    shutil.rmtree(zip_name)
+
+    with open(zip_name+'.zip', 'rb') as f:
+        st.download_button('Download Zip', f, file_name=zip_name+'.zip')
+except Exception:
+    pass
